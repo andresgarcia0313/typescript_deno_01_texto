7c071f5796ec406fd495c2f757a9ae30bf8b633a
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2025-02-24 22:00:51 -0500

    test: Agregar registro de resultado en la prueba de la función main

--- tests/main_test.ts
+++ tests/main_test.ts
@@ -2,5 +2,7 @@ import { assertEquals } from "std/assert/assert_equals.ts";
 import { main } from "../src/main.ts";
 
 Deno.test("La función main retorna la misma entrada", () => {
-  assertEquals( main("entrada"), "entrada");
+  const resultado = main("entrada");
+  console.log("Resultado:", resultado);
+  assertEquals(resultado, "entrada");
 });
